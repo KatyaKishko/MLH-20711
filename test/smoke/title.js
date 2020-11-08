8b41dec2c4fa77e5476de8fc3cb792f632c3737d
saglariamanova <sglrusa@gmail.com>
2020-11-07 16:11:23 -0800

    MLH-501 title test passed

--- test/smoke/title.js
+++ test/smoke/title.js
@@ -1,6 +1,5 @@
-//const sel = require ('../../data/selectors.json')
+
 const exp = require ('../../data/expected.json')
-//import {field, gender, story} from '../../data/helpers'
 
 describe('Getting to the page', function () {
 
