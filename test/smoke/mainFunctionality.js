1a3f40f8c1701f60b54bf4c9ae96ad138cf08e5c
Saglar Iamanova <49967802+PolinaBasta@users.noreply.github.com>
2020-11-07 18:15:17 -0800

    MLH-502 header tests passed (#2)

--- test/smoke/mainFunctionality.js
+++
@@ -1,4 +0,0 @@
-const sel = require ('../../data/selectors.json');
-const data = require ('../../data/testData.json');
-import {inputValues4} from '../../data/helpers'
-const path = require('path');
